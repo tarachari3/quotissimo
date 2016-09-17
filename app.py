113fc7817655f440948a1d3ec7eb92a5f946305c
Tara Chari <tarachari@dhcp-ccc-410.redrover.cornell.edu>
2016-09-16 23:38:46 -0400

    watson function

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
+# pip install --upgrade watson-developer-cloud
+import json
+from watson_developer_cloud import AlchemyLanguageV1
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    return 'Hello, World!'
+
+
+
+@app.route('/quoteGen')
+def quoteGen(userID):
+	alchemy_language = AlchemyLanguageV1(api_key='e06c74ac7872e80fbad8f78f7a670c662ecee9d1')
+print(json.dumps(alchemy_language.keywords(url='twitter.com/ibmwatson'),indent=2))
     return 'Hello, World!'
